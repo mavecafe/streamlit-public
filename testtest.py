d06a4055e3d12d0548d2b327740aa605d8ddc4ca
mavecafe <mavecafe@gmail.com>
2022-03-13 08:40:44 +0900

    pandasのデータを追加

--- testtest.py
+++ testtest.py
@@ -4,3 +4,12 @@ import streamlit as st
 st.title('テストサイト')
 
 st.write('ここは日本の結婚率と離婚率のページです')
+
+
+df = pd.DataFrame({
+    '都道府県':['東京都','千葉県','神奈川県','埼玉県'],
+    'lat':[1,2,3,4],
+    'lot':[10,20,30,40]
+})
+
+st.write(df)
