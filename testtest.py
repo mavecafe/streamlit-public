d973df1fcb50dd6f8bb87c996f8f3619b0d4553e
mavecafe <mavecafe@gmail.com>
2022-03-13 14:12:32 +0900

    11times

--- testtest.py
+++ testtest.py
@@ -58,9 +58,9 @@ df = pd.DataFrame({
 })
 st.map(df)
 
-#mrdata3 = pd.read_csv('streamlit-public/20200806mr_data3.csv',encoding='utf-8')
-mrdata3 = pd.read_csv('20200806mr_data3.csv',encoding='utf-8')
-st.table(mrdata3)
+# #mrdata3 = pd.read_csv('streamlit-public/20200806mr_data3.csv',encoding='utf-8')
+# mrdata3 = pd.read_csv('20200806mr_data3.csv',encoding='utf-8')
+# st.table(mrdata3)
 
 #df3 = pd.DataFrame(mrdata3,columns=['lat','lon'])
 
