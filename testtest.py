f189d7ded5fdb60270bc24475671c5c7ba30dce7
mavecafe <mavecafe@gmail.com>
2022-03-13 09:17:03 +0900

    csvのデータにUTF-8追加

--- testtest.py
+++ testtest.py
@@ -35,5 +35,5 @@ st.map(df)
 
 
 if st.checkbox('マンションデータ'):
-     mrdata = pd.read_csv('20200806mr_data1.csv',encoding='cp-932')
+     mrdata = pd.read_csv('20200806mr_data1.csv',encoding='cp932')
      st.table(mrdata)
